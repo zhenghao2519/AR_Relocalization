94e1c5d2a570c09f9ffb06b2b0826e483af12300
SuperSid94 <christiancsy94@gmail.com>
2023-01-28 20:55:11 +0100

    add a new scene (start menu)

--- Assets/Scripts/CaptureImageRunTimeManager.cs
+++ Assets/Scripts/CaptureImageRunTimeManager.cs
@@ -12,37 +12,17 @@ using System.Drawing;
 public class CaptureImageRunTimeManager : MonoBehaviour
 {
     [SerializeField]
+    private Text hintsLog;
+
+    [SerializeField]
     private Button captureImageButton;
 
-    [SerializeField]
-    private GameObject presentObject;
-
-    [SerializeField]
-    private XRReferenceImageLibrary runtimeImageLibrary;
-
-    [SerializeField]
-    private int MaxNumberOfMovingImages;
-
-    
-    private ARTrackedImageManager trackImageManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
-        //trackImageManager = gameObject.GetComponent<ARTrackedImageManager>();
-        trackImageManager =  gameObject.AddComponent<ARTrackedImageManager>();
-        
-        trackImageManager.referenceLibrary = runtimeImageLibrary;
-        //trackImageManager.referenceLibrary = trackImageManager.CreateRuntimeLibrary(runtimeImageLibrary);
-        trackImageManager.requestedMaxNumberOfMovingImages = MaxNumberOfMovingImages;
-        trackImageManager.trackedImagePrefab = presentObject;
-
-
-        trackImageManager.enabled = true;
-        trackImageManager.trackedImagesChanged += OnChanged;
-        //capturing images take possibly more than few frames, using startcorotine to handle this job
+      
+        hintsLog.text = "please capture 3 images\n";
         captureImageButton.onClick.AddListener(() => StartCoroutine(CaptureImage()));
         
     }
@@ -51,7 +31,7 @@ public class CaptureImageRunTimeManager : MonoBehaviour
     
     void OnDisable()
     {
-        trackImageManager.trackedImagesChanged -= OnChanged;
+        //trackImageManager.trackedImagesChanged -= OnChanged;
     }
 
     private IEnumerator CaptureImage()
@@ -63,6 +43,8 @@ public class CaptureImageRunTimeManager : MonoBehaviour
         string newName = Guid.NewGuid().ToString();
         //XRReferenceImage newImage = new XRReferenceImage(imageGuid, textureGuid, new Vector2(0.1f, 0.1f), newName, texture2D);
         StartCoroutine(SaveImage(texture, newName));
+
+        
     }
 
     
@@ -78,13 +60,15 @@ public class CaptureImageRunTimeManager : MonoBehaviour
         writer.Write(bytes);
         file.Close();
 
-        Debug.Log(path);
+        hintsLog.text = "please capture more images\n";
     }
 
     void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
+          
+
             // Display the name of the tracked image in the canvas
             //currentImageText.text = trackedImage.referenceImage.name;
             //trackedImage.transform.Rotate(Vector3.up, 180);
--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void MoveToScene(int sceneID)
+    public void MoveToScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneID);
+        SceneManager.LoadScene(sceneName);
+        //Scene newScene = SceneManager.GetSceneByName(sceneName);
+        //SceneManager.SetActiveScene(newScene);
     }
    
 }
