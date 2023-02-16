b808fcaf1fb77dee3e49fa1c675ab536d07af423
zhenghao.zhang <zhenghao2519@foxmail.com>
2023-02-16 21:41:36 +0100

    Store position/rotation and place cube

--- Assets/Scripts/ImageCapture.cs
+++ Assets/Scripts/ImageCapture.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Drawing;
+using Unity.XR.CoreUtils;
 //using Application = UnityEngine.Application;
 
 public class CaptureImageRunTimeManager : MonoBehaviour
@@ -18,17 +19,19 @@ public class CaptureImageRunTimeManager : MonoBehaviour
     private Button captureImageButton;
 
     [SerializeField]
-    private Button goToStartMenuButton;
+    private Button placeCubeButton;
 
     [SerializeField]
     private InputField inputField;
 
     private int imageCounter = 0;
+    private Transform xrCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         hintsLog.text = "Please first enter a location name! ";
+        xrCamera = GameObject.Find("XR Origin").transform.Find("Camera Offset").Find("Main Camera");
         captureImageButton.onClick.AddListener(() => StartCoroutine(CaptureImage()));
         
     }
@@ -42,8 +45,13 @@ public class CaptureImageRunTimeManager : MonoBehaviour
         // Deactive the input field once the capture button is pressed
         inputField.gameObject.SetActive(false);
         var texture = ScreenCapture.CaptureScreenshotAsTexture();
-
-        string newName = "["+inputField.text+"]"+Guid.NewGuid().ToString();
+        //The camera's local position in xr origin space.
+        Vector3 imagePosition = xrCamera.position;
+        Vector3 imageRotation = xrCamera.eulerAngles;
+        //Save the jpeg with name in the form of
+        //[LocationName]{ImagePosition}`ImageRotation`UniqueIdentifier
+        string newName = "["+inputField.text+"]"+"{" + imagePosition.ToString() + "}" + "`" + imageRotation.ToString() + "`" + Guid.NewGuid().ToString();
+        //string newName = Guid.NewGuid().ToString();
         //XRReferenceImage newImage = new XRReferenceImage(imageGuid, textureGuid, new Vector2(0.1f, 0.1f), newName, texture2D);
         StartCoroutine(SaveImage(texture, newName));
         
@@ -53,7 +61,7 @@ public class CaptureImageRunTimeManager : MonoBehaviour
 
     public IEnumerator SaveImage(Texture2D texture2D, string newName)
     {   
-        String path = Application.persistentDataPath + "/temp";
+        string path = Application.persistentDataPath + "/temp";
         yield return null;
         byte[] bytes = texture2D.EncodeToJPG();
         if (!Directory.Exists(path))
@@ -66,7 +74,6 @@ public class CaptureImageRunTimeManager : MonoBehaviour
 
      void Update()
     {
-        Debug.Log(imageCounter);
         if (imageCounter == 1)
         {
             hintsLog.text = "Take at least 3 pictures.";
@@ -74,7 +81,8 @@ public class CaptureImageRunTimeManager : MonoBehaviour
         if (imageCounter == 3)
         {
             hintsLog.text = "Enough pictures taken!";
-            goToStartMenuButton.gameObject.SetActive(true);
+            placeCubeButton.gameObject.SetActive(true);
+            
         }
     }
 
---
+++ Assets/Scripts/TapToPlaceCube.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlaceCube : MonoBehaviour
+{
+    public GameObject gameObjectToPlace;
+
+    
+    private GameObject spawnedObject;
+    private ARRaycastManager arRaycastManager;
+    private Vector2 touchPosition;
+    private string path;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        path = Application.persistentDataPath + "/temp/cube";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition) {
+        if(Input.touchCount > 0) {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition)) {
+            return;
+        }
+
+        if (arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon)) {
+            var hitPose = hits[0].pose;
+
+            if(spawnedObject == null) {
+                spawnedObject = Instantiate(gameObjectToPlace, hitPose.position, hitPose.rotation);
+                StartCoroutine(SaveJson());
+                //} else {
+                //    spawnedObject.transform.position = hitPose.position;
+            }
+           
+        }
+    }
+
+    public IEnumerator SaveJson() {
+        yield return null;
+        //string json = JsonUtility.ToJson(spawnedObject.transform);
+        string json = "{"+ spawnedObject.transform.position.ToString()+"}"+ "`" + spawnedObject.transform.eulerAngles.ToString() + "`";
+        //File.Create(path + "/cube.txt");
+        //File.WriteAllText(path + "/cube.txt", json, System.Text.Encoding.UTF8);
+        FileStream fs = new FileStream(path + "/cube.json", FileMode.Create);
+        byte[] bytes = new UTF8Encoding().GetBytes(json.ToString());
+        fs.Write(bytes, 0, bytes.Length);
+        fs.Close();
+        //String path = Application.persistentDataPath + "/temp";
+        //yield return null;
+        //if (!File.Exists(path + "/cubeJson.txt"))
+        //    File.Create(path + "/cubeJson.txt");
+        //FileStream file = File.Open(path + "/cubeJson.txt", FileMode.Open);
+        //StreamWriter writer = new StreamWriter(file);
+        //writer.Write(json);
+        //file.Close();
+    }
+}
